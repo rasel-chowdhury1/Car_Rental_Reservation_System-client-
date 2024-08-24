0fc2b87f5cda68e13fa44f0a9d42a62396ff234c
rasel-chowdhury1 <rasel.chowdhury0001@gmail.com>
2024-08-24 21:30:00 +0600

    react,typescript and tailwind setup done

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <h1 className="text-3xl font-bold text-center ">
+      Car Rental Reservation System
+    </h1>
+  )
+}
+
+export default App
