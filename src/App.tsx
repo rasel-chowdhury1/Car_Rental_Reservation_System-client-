8cc178112d09d5a5bc9e16342d25c349c633d3cb
rasel-chowdhury1 <rasel.chowdhury0001@gmail.com>
2024-08-25 14:16:11 +0600

    project initial setup done

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 
-import './App.css'
-
 function App() {
 
   return (
-    <h1 className="text-3xl font-bold text-center ">
-      Car Rental Reservation System
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
     </h1>
   )
 }
